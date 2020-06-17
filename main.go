2c0e2e6d743e4f790afca60a5ab765950abf2504
Regina Scott <50851526+reginapizza@users.noreply.github.com>
2020-06-17 15:22:03 -0400

    Added flags for token, cluster, and namespace (#7)
    * testing pull request
    
    * added flags
    
    Co-authored-by: Regina Scott <rescott@localhost.localdomain>

--- main.go
+++ main.go
@@ -17,10 +17,29 @@ var versionCommand = &cobra.Command{
 	},
 }
 
+var (
+	cluster   string
+	namespace string
+	token     string
+)
+
 func init() {
 	rcommand.AddCommand(versionCommand)
+	rcommand.AddCommand(listCommand)
+	rcommand.PersistentFlags().StringVarP(&cluster, "server", "s", "myurl", "this is the cluster url")
+	rcommand.PersistentFlags().StringVarP(&token, "token", "t", "usertoken", "this is the user token")
+	rcommand.PersistentFlags().StringVarP(&namespace, "namespace", "n", "namespace", "this is the namespace")
+	rcommand.MarkFlagRequired("server")
 }
+
 func main() {
-	fmt.Println("Hello World")
 	rcommand.Execute()
+}
+
+var listCommand = &cobra.Command{
+	Use: "list",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("List All Kubernetes Applications")
+		fmt.Printf("Fetching all applications from %s in namespace %s", cluster, namespace)
+	},
 }
