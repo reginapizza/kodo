54c068ce13a54d7f0750bc32c49d1268413fee1f
Shoubhik Bose <shbose@redhat.com>
2020-06-18 00:56:32 -0400

    upgrade client-go to kubernetes 0.18.3 (#12)

--- pkg/kodo/cmd/list.go
+++ pkg/kodo/cmd/list.go
@@ -1,16 +1,17 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 //List is a function to list number of pods in the cluster
 //List
 func List() error {
 	client := newOpenShiftClient()
-	pods, _ := client.CoreV1().Pods("").List(v1.ListOptions{})
+	pods, _ := client.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
 	fmt.Printf("\n The number of pods are %d \n", len(pods.Items))
 	return nil
 }
