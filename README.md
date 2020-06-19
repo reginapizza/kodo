64219fc59252c8aecd6b4e31e455433da27b8f1c
Shraddha Kharche <skharche@localhost.localdomain>
2020-06-19 15:57:52 -0400

    Updated readme file,updated mandatory flags

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# kodo
-##### testing
+**<h1>Command line utilities using cobra and Go</h1>**
+>A go program to develop command line utilities with commands, subcommands, aliase etc.
+
+**Dependencies**
+> "github.com/spf13/cobra" <br />
+> "k8s.io/client-go/kubernetes" <br />
+> "k8s.io/client-go/rest" <br />
+> "k8s.io/apimachinery/pkg/apis/meta/v1" <br />
+> "github.com/openshift/api/route/v1" <br />
+> "github.com/openshift/client-go/route/clientset/versioned/typed/route/v1" <br />
+> "k8s.io/api/apps/v1" <br />
+> "k8s.io/api/core/v1" <br />
+> "k8s.io/apimachinery/pkg/apis/meta/v1" <br />
+
+**How to Install**
+> git clone https://github.com/cli-playground/kodo.git
+> cd kodo
+
+**Commands List**<br/>
+Command to build project -> go build -o bin/kodo main.go<br/>
+To try out different commands,go to folder bin/kodo and try running below commands<br />
+>  count       command to count <resources><br />
+>  deploy      Command to deploy an image<br />
+>  help        Help about available commands<br />
+>  version     Version details<br />
+  
--- main.go
+++ main.go
@@ -23,22 +23,37 @@ var versionCommand = &cobra.Command{
 
 func init() {
 	rcommand.AddCommand(versionCommand)
-	rcommand.AddCommand(listCommand)
+	rcommand.AddCommand(countCommand)
+	countCommand.AddCommand(podCommand)
 	rcommand.AddCommand(deployCommand)
-	rcommand.PersistentFlags().StringVarP(&envVar.Host, "server", "s", "myurl", "this is the cluster url")
-	rcommand.PersistentFlags().StringVarP(&envVar.Bearertoken, "token", "t", "usertoken", "this is the user token")
-	rcommand.PersistentFlags().StringVarP(&envVar.Namespace, "namespace", "n", "", "this is the namespace")
+
+	deployCommand.Flags().StringVarP(&envVar.Host, "server", "s", "", "this is the cluster url")
+	deployCommand.Flags().StringVarP(&envVar.Bearertoken, "token", "t", "", "this is the user token")
+	deployCommand.Flags().StringVarP(&envVar.Namespace, "namespace", "n", "", "this is the namespace")
 
 	/* Changed default namespace from 'namespace' to "" because
 	'namespace' causes some internal error and returns 0 pods
 	even if login credentials correct but namespace not explicitly set
 	whereas "" works fine */
 
-	rcommand.PersistentFlags().StringVarP(&deployVar.Image, "image", "i", "myimage", "this is the tagged image for deployment")
-	rcommand.PersistentFlags().Int32VarP(&deployVar.Replicas, "replicas", "r", 1, "number of replicas")
-	rcommand.PersistentFlags().Int32VarP(&deployVar.Port, "port", "p", 8000, "port at which app should run")
+	//Flag for individual command
 
-	rcommand.MarkFlagRequired("server")
+	deployCommand.Flags().StringVarP(&deployVar.Image, "image", "i", "myimage", "tagged image for deployment")
+	deployCommand.Flags().Int32VarP(&deployVar.Replicas, "replicas", "r", 1, "number of replicas")
+	deployCommand.Flags().Int32VarP(&deployVar.Port, "port", "p", 8000, "port at which app should run")
+
+	//required attributes for deploy command
+	deployCommand.MarkFlagRequired("server")
+	deployCommand.MarkFlagRequired("token")
+	deployCommand.MarkFlagRequired("image")
+
+	podCommand.Flags().StringVarP(&envVar.Host, "server", "s", "", "this is the cluster url")
+	podCommand.Flags().StringVarP(&envVar.Bearertoken, "token", "t", "", "this is the user token")
+	podCommand.Flags().StringVarP(&envVar.Namespace, "namespace", "n", "", "this is the namespace")
+	//required parameters for count pods command
+	podCommand.MarkFlagRequired("server")
+	podCommand.MarkFlagRequired("token")
+
 }
 
 func main() {
@@ -46,18 +61,23 @@ func main() {
 	rcommand.Execute()
 }
 
-var listCommand = &cobra.Command{
-	Use: "list",
+var countCommand = &cobra.Command{
+	Use:   "count",
+	Short: "Command to count <resources>",
+}
+
+var podCommand = &cobra.Command{
+	Use: "pods",
 	Run: func(cm *cobra.Command, args []string) {
 		fmt.Println("List All Kubernetes Applications")
 		fmt.Printf("\nFetching all applications from %s in namespace %s", envVar.Host, envVar.Namespace)
 		cmd.List(envVar)
-
 	},
 }
 
 var deployCommand = &cobra.Command{
-	Use: "deploy",
+	Use:   "deploy",
+	Short: "Command to deploy an image",
 	Run: func(cm *cobra.Command, args []string) {
 		fmt.Println("Creating image deployment")
 		cmd.Deploy(deployVar, envVar)
